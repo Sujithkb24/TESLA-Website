12eb68a2a831db5dbcfbe4f0c613ce5c3a121490
unknown <sujithkb24@gmail.com>
2025-12-11 00:03:54 +0530

    change

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const router = require('./routes');
-
+const teamRoutes = require('./routes/teamRoutes');
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
 
 app.use('/api', router);
-
+app.use('/api/teams', teamRoutes);
 app.get('/', (req, res) => res.json({ status: 'ok' }));
 
 module.exports = app;
